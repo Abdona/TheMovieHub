63b9eb4727f3eeda5bcf0da4361a4d748769581d
Abdulrahman <boodynaser@rocketmail.com>
2021-08-04 17:08:38 +0200

    Increase spacing between elements

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { getPopular } from './API.js';
 import { Count, creatHtml } from './DomManipulation.js';
 
 (async () => {
-  const movielist = await getPopular(6);
+  const movielist = await getPopular(10);
   creatHtml(movielist);
   Count(movielist);
 })();
