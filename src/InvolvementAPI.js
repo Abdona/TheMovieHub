6fa16330e20d3d478454987eb7c39178367be936
Abdulrahman <boodynaser@rocketmail.com>
2021-08-06 01:27:54 +0200

    Renew Involvment API appkey

--- src/InvolvementAPI.js
+++ src/InvolvementAPI.js
@@ -1,13 +1,6 @@
-// export const createMovie = async () => {
-//   const resp = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/', {
-//     method: 'POST',
-//   });
-//   return resp.text();
-// };
-
 export const createMovielike = async (itemId) => {
   const data = JSON.stringify({ item_id: itemId });
-  const resp = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/R28OTC179PUdUdjaPoCa/likes/', {
+  const resp = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/pk9RMX2Es1GYRfVPhxJy/likes/', {
     method: 'POST',
     headers: {
       'Content-type': 'application/json; Charset=UTF-8',
@@ -18,14 +11,14 @@ export const createMovielike = async (itemId) => {
 };
 
 export const getMovielike = async () => {
-  const resp = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/R28OTC179PUdUdjaPoCa/likes/', {
+  const resp = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/pk9RMX2Es1GYRfVPhxJy/likes/', {
   });
   return resp.text();
 };
 
 export const createMoviecomment = async (id, userName, userComment) => {
   const data = JSON.stringify({ item_id: id, username: userName, comment: userComment });
-  const resp = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/R28OTC179PUdUdjaPoCa/comments/', {
+  const resp = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/pk9RMX2Es1GYRfVPhxJy/comments/', {
     method: 'POST',
     headers: {
       'Content-type': 'application/json; Charset=UTF-8',
@@ -36,10 +29,7 @@ export const createMoviecomment = async (id, userName, userComment) => {
 };
 
 export const getMoviecomment = async (itemId) => {
-  const resp = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/R28OTC179PUdUdjaPoCa/comments?item_id=${itemId}`, {
+  const resp = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/pk9RMX2Es1GYRfVPhxJy/comments?item_id=${itemId}`, {
   });
   return resp.text();
 };
-// this function returns
-// [{"comment":"Das is wunderbar","creation_date":"2021-08-04","username":"abdulrahman"},
-// {"username":"abdulrahman","creation_date":"2021-08-04","comment":"Das is wunderbar"}]
