4d89800697b5b4f5ade72b6bca470516dc3c59e5
Abdulrahman <boodynaser@rocketmail.com>
2021-08-06 15:10:25 +0200

    Revert "decrease number of movies fetched from the api"
    This reverts commit 8fea942a89a35a9b0e44f1b6d2ff461bbefadc83.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { getPopular } from './API.js';
 import { Count, creatHtml, headerMoviecount } from './DomManipulation.js';
 
 (async () => {
-  const movielist = await getPopular(6);
+  const movielist = await getPopular(12);
   creatHtml(movielist);
   headerMoviecount(Count(movielist));
 })();
